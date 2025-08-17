9d9ed7e6714fc174e6f27b0868b13ef72460aa13
Guto Scheneider <jascheneider@gmail.com>
2025-08-17 16:45:59 -0300

    notificacao em arquivo e console

--- src/interfaces/Notificacao.java
+++ src/interfaces/Notificacao.java
@@ -1,13 +1,5 @@
 package src.interfaces;
 
-public class Notificacao {
-    private String message;
-
-    public Notificacao(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+public interface Notificacao {
+    void notificar(String mensagem);
 }
